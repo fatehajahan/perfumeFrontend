5b9ae6bc54f35a56ac8ce0575964a64c833ce9a2
Fatehajahan2002 <fatehajahan2002@gmail.com>
2025-07-18 00:48:57 +0600

    done

--- src/mainPage/AboutUs/AboutUs.jsx
+++ src/mainPage/AboutUs/AboutUs.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import aboutImg1 from '../../assets/aboutUs/bg-14.jpg'
+import fixedImg from '../../assets/aboutUs/bg-13.jpg'
 
 const AboutUs = () => {
     return (
@@ -21,9 +22,23 @@ const AboutUs = () => {
                 <p>Nunc vulputate feugiat tortor ac congue. Nam sit amet urna vitae ligula pellentesque porttitor ut id nisl. Nam laoreet velit ipsum. Suspendisse ullamcorper, odio id faucibus consectetur, elit quam cursus est, nec sagittis purus enim a eros. Sed posuere nec lacus sit amet ullamcorper. Donec ut egestas purus. Duis placerat turpis non urna convallis convallis vitae nec odio. Vestibulum quam turpis, blandit vel fermentum sed, pulvinar in risus. Etiam a mauris sed nunc venenatis vulputate vitae quis tortor. Curabitur gravida accumsan elementum. Praesent ut hendrerit dui, vitae elementum mi.</p>
             </div>
 
-            <div className="aboutContainer sm:aboutContainer lg:aboutContainer xl:aboutContainer">
+            <div className="container">
                 <img src={aboutImg1} alt="" />
             </div>
+
+            <div className='py-[100px]'>
+                <div className='container'>
+                    <div className=' uppercase font-bold font-cormot'>
+                        <p>Old friends</p>
+                        <h2 className='text-[42px]  font-normal'>here with you for over 15 years</h2>
+                    </div>
+                    <p className='text-[#5b5b5b]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ultrices posuere magna, et cursus metus scelerisque congue. Maecenas lorem enim, pharetra sed imperdiet eu, bibendum id nibh. In hac habitasse platea dictumst. Donec mattis molestie arcu vitae tempus. In hac habitasse platea dictumst. Ut mattis mauris vitae justo accumsan dignissim.
+
+                        Ut egestas quam sit amet magna sollicitudin iaculis. In sit amet libero vel mauris bibendum faucibus ac non diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin id ante pulvinar risus convallis consectetur et sit amet erat. Aliquam et sapien rhoncus, vulputate urna non, egestas elit. In lacus ante, imperdiet eget justo ut, sodales sollicitudin turpis. Donec egestas mauris vel enim facilisis, nec dictum mi consectetur. Suspendisse ac ullamcorper felis. Nulla id libero id justo lacinia consectetur sit amet ac libero. Maecenas cursus placerat auctor. Integer condimentum elit ut feugiat ornare. Maecenas leo ipsum, pharetra sed ornare id, mattis vel nisi. Nam dui nunc, gravida sed tortor non, tempus pretium lectus.</p>
+                </div>
+            </div>
+
+            <div className='bg-aboutFixed bg-cover bg-no-repeat bg-center md:pt-[100px] md:pb-[80px] h-screen flex items-center md:justify-center md:bg-fixed'></div>
         </div>
     )
 }
